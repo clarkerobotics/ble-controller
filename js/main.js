bbf2becc89a336709e10cae0739459da6a137bc5
Trevor Clarke <besnoid@gmail.com>
2017-10-03 23:25:36 -0700

    Initial setup of files!

---
+++ js/main.js
@@ -0,0 +1,86 @@
+// NOTES:
+// D28 - Button! :D
+// D2 - right pot
+// D31 - right pot
+// D29 - left pot
+// D30 - left pot
+var called = false;
+
+function onInit() {
+if (called) return;
+called = true;
+var on = false;
+var button = D28;
+var leftUD = D29;
+var leftLR = D30;
+var rightUD = D31;
+var rightLR = D2;
+var timer;
+
+//reset all
+LED1.write(0); // RED
+LED2.write(0); // GREEN
+LED3.write(0); // BLUE
+
+function sendData(data) {
+  //console.log("sendData", data);
+  // TODO: send as JSON string
+  var str = JSON.stringify(data);
+  console.log("sendData", str);
+}
+
+setWatch(function(e) {
+  on = !on;
+  LED1.write(on);
+  if (on) setTimeout(function(){
+    LED1.write(0);
+  }, 500);
+  console.log("Enabled: ", on);
+  sendData({
+    e: on,
+    lud: 0,
+    llr: 0,
+    rud: 0,
+    rlr: 0
+  });
+  if (on) startController();
+  if (!on) clearInterval(timer);
+}, button, { repeat: true, edge: 'rising', debounce: 50 });
+// you'll need to use  edge: 'falling' if you connected the button to 0v
+
+function startController(){
+  timer = setInterval(function(){
+    var l1 = analogRead(leftUD);
+    var l2 = analogRead(leftLR);
+    var r1 = analogRead(rightUD);
+    var r2 = analogRead(rightLR);
+
+    //format values
+    l1 = Math.round(Math.abs(l1 * 1000));
+    l2 = Math.round(Math.abs(l2 * 1000));
+    r1 = Math.round(Math.abs(r1 * 1000));
+    r2 = Math.round(Math.abs(r2 * 1000));
+    var leftLedOn = 0;
+    var rightLedOn = 0;
+    if (l1 > 900 || l1 < 10) leftLedOn = 1;
+    if (r1 > 900 || r1 < 10) rightLedOn = 1;
+    LED2.write(leftLedOn);
+    LED3.write(rightLedOn);
+    //console.log('vars',l1, l2, r1, r2);
+
+    // Send over the wire!
+    sendData({
+      e: true,
+      lud: l1,
+      llr: l2,
+      rud: r1,
+      rlr: r2
+    });
+  }, 300);
+}
+
+}
+onInit();
+
+// NOTE: Use when needing to store in memory! start from memory!
+//save()
